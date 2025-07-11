e5696e7fc6043143742ea07c79e683d49ece2b64
yuhyun1 <yh7119@gmail.com>
2025-07-11 17:06:25 +0900

    feat: 선편지 엔티티 생성

---
+++ src/main/java/com/rainbowletter/server/petinitiatedletter/application/domain/model/PetInitiatedLetter.java
@@ -0,0 +1,62 @@
+package com.rainbowletter.server.petinitiatedletter.application.domain.model;
+
+import com.rainbowletter.server.ai.application.domain.model.AiPrompt;
+import com.rainbowletter.server.ai.application.domain.model.AiPrompt.PromptType;
+import com.rainbowletter.server.common.adapter.out.persistence.BaseTimeJpaEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Table(name = "pet_initiated_letter")
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = PROTECTED)
+public class PetInitiatedLetter extends BaseTimeJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long petId;
+
+    @NotNull
+    @Column(length = 20)
+    private String summary;
+
+    @NotNull
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @NotNull
+    @Column(columnDefinition = "TEXT")
+    private String promptA;
+
+    @NotNull
+    @Column(columnDefinition = "TEXT")
+    private String promptB;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private PromptType promptType;
+
+    private LocalDateTime submitTime;
+
+    @NotNull
+    private boolean read;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private PetInitiatedLetterStatus status;
+
+}
