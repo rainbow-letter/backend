fdef0d833a29a3842b28b4340faea86cc0e52942
yuhyun1 <yh7119@gmail.com>
2025-07-16 15:38:49 +0900

    feat: 선편지-펫 등록 기능 추가

---
+++ src/main/java/com/rainbowletter/server/petinitiatedletter/adapter/out/persistence/UserPetInitiatedLetterJpaRepository.java
@@ -0,0 +1,8 @@
+package com.rainbowletter.server.petinitiatedletter.adapter.out.persistence;
+
+import com.rainbowletter.server.petinitiatedletter.application.domain.model.UserPetInitiatedLetter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserPetInitiatedLetterJpaRepository extends JpaRepository<UserPetInitiatedLetter, Long> {
+    boolean existsByUserIdAndPetId(Long userId, Long petId);
+}
