42587769784c93fb767a8c5c7d4e3a9a9fd0a531
yuhyun1 <yh7119@gmail.com>
2025-07-18 13:51:07 +0900

    feat: 편지 리스트 조회 시 sequence 응답값 제거 및 선편지 응답값 추가

--- src/main/java/com/rainbowletter/server/letter/adapter/in/web/dto/LetterCollectResponse.java
+++ src/main/java/com/rainbowletter/server/letter/adapter/in/web/dto/LetterCollectResponse.java
@@ -1,14 +1,16 @@
 package com.rainbowletter.server.letter.adapter.in.web.dto;
 
 import com.rainbowletter.server.letter.adapter.out.dto.PaginationInfo;
-import com.rainbowletter.server.letter.application.port.in.dto.LetterSimpleWithSequenceResponse;
+import com.rainbowletter.server.letter.application.port.in.dto.LetterSimpleResponse;
 import com.rainbowletter.server.pet.application.port.in.dto.PetSummary;
+import com.rainbowletter.server.petinitiatedletter.adapter.in.web.dto.PetInitiatedLetterSimpleResponse;
 
 import java.util.List;
 
 public record LetterCollectResponse(
     PetSummary petSummary,
-    List<LetterSimpleWithSequenceResponse> letters,
+    List<LetterSimpleResponse> letters,
+    List<PetInitiatedLetterSimpleResponse> petLetters,
     PaginationInfo paginationInfo
 ) {
 }
---
+++ src/main/java/com/rainbowletter/server/petinitiatedletter/adapter/in/web/dto/PetInitiatedLetterSimpleResponse.java
@@ -0,0 +1,11 @@
+package com.rainbowletter.server.petinitiatedletter.adapter.in.web.dto;
+
+import java.time.LocalDateTime;
+
+public record PetInitiatedLetterSimpleResponse(
+    Long id,
+    LocalDateTime createdAt,
+    String summary,
+    String content
+) {
+}
