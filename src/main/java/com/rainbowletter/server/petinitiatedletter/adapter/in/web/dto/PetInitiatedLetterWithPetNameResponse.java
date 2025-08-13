6994c4ab230015c1ddec10366ce493893149cc6d
yuhyun1 <yh7119@gmail.com>
2025-08-13 15:43:15 +0900

    feat: 공유링크 - 선편지 상세 조회 기능 추가

---
+++ src/main/java/com/rainbowletter/server/petinitiatedletter/adapter/in/web/dto/PetInitiatedLetterWithPetNameResponse.java
@@ -0,0 +1,14 @@
+package com.rainbowletter.server.petinitiatedletter.adapter.in.web.dto;
+
+import java.time.LocalDateTime;
+
+public record PetInitiatedLetterWithPetNameResponse(
+    Long letterId,
+    LocalDateTime createdAt,
+    LocalDateTime submitTime,
+    String summary,
+    String content,
+    Long petId,
+    String petName
+) {
+}
