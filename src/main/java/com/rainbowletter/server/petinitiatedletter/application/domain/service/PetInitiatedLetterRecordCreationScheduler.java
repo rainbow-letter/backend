2a8b9bf1a66f90e40add5a6d6ac96603b68ed165
yuhyun1 <yh7119@gmail.com>
2025-08-18 16:45:49 +0900

    feat: 선편지 리스트 생성 스케줄링 추가 + GPT 호출 및 편지 내용 생성 로직 추가 TODO : 트랜잭션 분리, 외부 API 호출 실패 시 예외 처리

---
+++ src/main/java/com/rainbowletter/server/petinitiatedletter/application/domain/service/PetInitiatedLetterRecordCreationScheduler.java
@@ -0,0 +1,59 @@
+package com.rainbowletter.server.petinitiatedletter.application.domain.service;
+
+import com.rainbowletter.server.ai.application.domain.model.AiSetting;
+import com.rainbowletter.server.ai.application.port.out.LoadSettingPort;
+import com.rainbowletter.server.petinitiatedletter.adapter.out.persistence.PetInitiatedLetterJpaRepository;
+import com.rainbowletter.server.petinitiatedletter.adapter.out.persistence.UserPetInitiatedLetterPersistenceAdapter;
+import com.rainbowletter.server.petinitiatedletter.application.domain.model.GeneratePetInitiatedLetterEvent;
+import com.rainbowletter.server.petinitiatedletter.application.domain.model.PetInitiatedLetter;
+import com.rainbowletter.server.petinitiatedletter.application.domain.model.PetInitiatedLetterStatus;
+import com.rainbowletter.server.petinitiatedletter.application.port.in.dto.UserPetPairDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class PetInitiatedLetterRecordCreationScheduler {
+
+    private final UserPetInitiatedLetterPersistenceAdapter userPetInitiatedLetterPersistenceAdapter;
+    private final PetInitiatedLetterJpaRepository petInitiatedLetterJpaRepository;
+    private final ApplicationEventPublisher eventPublisher;
+    private final LoadSettingPort loadSettingPort;
+
+    @Async
+    @Scheduled(cron = "0 30 19 * * MON,WED,FRI")
+    @Transactional
+    public void createPetInitiatedLetterRecords() {
+        List<UserPetPairDto> userPetPairs = userPetInitiatedLetterPersistenceAdapter.findAllUserPetPairs();
+
+        AiSetting aiSetting = loadSettingPort.loadPetInitiatedLetterSetting();
+
+        List<PetInitiatedLetter> letters = userPetPairs.stream()
+            .map(dto -> PetInitiatedLetter.builder()
+                .userId(dto.userId())
+                .petId(dto.petId())
+                .shareLink(UUID.randomUUID())
+                .promptType(aiSetting.getSelectPrompt())
+                .status(PetInitiatedLetterStatus.SCHEDULED)
+                .readStatus(false)
+                .build()
+            )
+            .toList();
+
+        List<PetInitiatedLetter> letterList = petInitiatedLetterJpaRepository.saveAll(letters);
+
+        eventPublisher.publishEvent(
+            new GeneratePetInitiatedLetterEvent(
+                letterList.stream().map(PetInitiatedLetter::getId).toList()
+            )
+        );
+    }
+
+}
