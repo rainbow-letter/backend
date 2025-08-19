a3929d588f4b045c01da0ae6e56fa31b214e15b5
yuhyun1 <yh7119@gmail.com>
2025-08-19 11:00:22 +0900

    fix: 편지함 조회 시 letterStatus를 통해 읽음 처리 확인 가능하게끔 수정

--- src/main/java/com/rainbowletter/server/reply/application/domain/service/ReadReplyService.java
+++ src/main/java/com/rainbowletter/server/reply/application/domain/service/ReadReplyService.java
@@ -1,6 +1,8 @@
 package com.rainbowletter.server.reply.application.domain.service;
 
 import com.rainbowletter.server.common.annotation.UseCase;
+import com.rainbowletter.server.letter.application.domain.model.Letter;
+import com.rainbowletter.server.letter.application.port.out.LoadLetterPort;
 import com.rainbowletter.server.reply.application.domain.model.Reply;
 import com.rainbowletter.server.reply.application.domain.model.Reply.ReplyId;
 import com.rainbowletter.server.reply.application.port.in.ReadReplyUseCase;
@@ -15,12 +17,15 @@ import org.springframework.transaction.annotation.Transactional;
 class ReadReplyService implements ReadReplyUseCase {
 
     private final LoadReplyPort loadReplyPort;
+    private final LoadLetterPort loadLetterPort;
     private final SaveReplyPort saveReplyPort;
 
     @Override
     public void readReply(final ReplyId replyId) {
         final Reply reply = loadReplyPort.loadReplyById(replyId);
+        final Letter letter = loadLetterPort.loadLetterById(reply.getLetterId());
         reply.read();
+        letter.read();
         saveReplyPort.save(reply);
     }
 
