be3fdbc88984c1acd14de867ecd56d6f6a563356
yuhyun1 <yh7119@gmail.com>
2025-08-26 15:08:38 +0900

    fix: 알림톡 템플릿 변경

--- src/main/java/com/rainbowletter/server/notification/application/domain/model/alimtalk/AlimTalkTemplateCode.java
+++ src/main/java/com/rainbowletter/server/notification/application/domain/model/alimtalk/AlimTalkTemplateCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AlimTalkTemplateCode {
     REPLY("TX_0503"),
-    PET_INITIATED_LETTER("UB_6127"),
+    PET_INITIATED_LETTER("UB_8222"),
     ;
 
     private final String code;
