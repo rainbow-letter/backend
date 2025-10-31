5aecaece7a52d2c7e3db162a4ed7136ee9e26030
yuhyun1 <yh7119@gmail.com>
2025-10-31 13:26:12 +0900

    fix: 선편지 기능 금요일 고정 생성

--- src/main/java/com/rainbowletter/server/petinitiatedletter/application/domain/service/PetInitiatedLetterRecordCreationScheduler.java
+++ src/main/java/com/rainbowletter/server/petinitiatedletter/application/domain/service/PetInitiatedLetterRecordCreationScheduler.java
@@ -27,7 +27,7 @@ public class PetInitiatedLetterRecordCreationScheduler {
     private final LoadSettingPort loadSettingPort;
 
 
-    @Scheduled(cron = "0 0 19 * * MON,WED,FRI")
+    @Scheduled(cron = "0 0 19 * * FRI")
     @Transactional
     public void createPetInitiatedLetterRecords() {
         List<UserPetPairDto> userPetPairs = userPetInitiatedLetterPersistenceAdapter.findAllUserPetPairs();
