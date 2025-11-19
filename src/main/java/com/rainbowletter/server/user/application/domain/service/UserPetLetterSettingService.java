cad8515e0fe3cc69a8c74c563b5837b0116041d3
yuhyun1 <yh7119@gmail.com>
2025-11-19 17:43:03 +0900

    fix: 선편지 생성 시간 변경

--- src/main/java/com/rainbowletter/server/user/application/domain/service/UserPetLetterSettingService.java
+++ src/main/java/com/rainbowletter/server/user/application/domain/service/UserPetLetterSettingService.java
@@ -104,7 +104,7 @@ public class UserPetLetterSettingService {
 
     private void validateNotInRestrictedTime(String email, LocalDateTime now) {
         if (isBlockedTime(now)) {
-            throw new RainbowLetterException("현재 시간에는 선편지 설정 변경이 불가능합니다. (월/수/금 19:00~21:00 제한)", email);
+            throw new RainbowLetterException("현재 시간에는 선편지 설정 변경이 불가능합니다. (15:00~20:00 제한)", email);
         }
     }
 
@@ -114,7 +114,7 @@ public class UserPetLetterSettingService {
         int minute = now.getMinute();
 
         boolean isBlockedDay = day == DayOfWeek.FRIDAY;
-        boolean isBlockedTime = (hour == 19) || (hour == 21 && minute == 0);
+        boolean isBlockedTime = (hour == 15) || (hour == 20 && minute == 0);
 
         return isBlockedDay && isBlockedTime;
     }
